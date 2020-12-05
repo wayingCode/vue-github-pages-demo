9426fb10a612e7c5ccd93cc4663a92bb4af3bdb1
hyoung <719220259@qq.com>
2020-12-05 13:07:20 +0800

    'h1'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome github pages demo"/>
   </div>
 </template>
